6200cdff60f1d779a1b30527f8b10bdf49b3b1ee
Vipuld14 <vipulsvdhotre@icloud.com>
2026-04-30 11:26:15 -0400

    changes made

--- js/config.js
+++ js/config.js
@@ -5,5 +5,5 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://ubtipilbddbgnpzbsdvp.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVidGlwaWxiZGRiZ25wemJzZHZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NjQxMTcsImV4cCI6MjA5MjQ0MDExN30.smYjBLH1rdZcd7SKcdRSwfdxlk5tkBNYljJ1q9zw8tAASE_ANON_KEY';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVidGlwaWxiZGRiZ25wemJzZHZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NjQxMTcsImV4cCI6MjA5MjQ0MDExN30.smYjBLH1rdZcd7SKcdRSwfdxlk5tkBNYljJ1q9zw8tA';
 const ADMIN_PASSWORD = '121814'; // Change this & update in Supabase admin_settings table
